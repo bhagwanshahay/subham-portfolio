1353df6ab2a9c1a898ebca93c136697e282d9825
Bhagwan Shahay <bhagwanshahay.dev@gmail.com>
2026-08-01 12:47:21 +0530

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-black text-white h-screen flex items-center justify-center">
+      <h1 className="text-6xl font-bold text-red-500">
+        Tailwind Working 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
