9d905602a9e8652e27fc3618e1c7e68055ad6ce5
Bhagwan Shahay <bhagwanshahay.dev@gmail.com>
2026-08-01 18:39:44 +0530

    Integrate Lenis smooth scrolling

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ReactLenis } from "lenis/react";
+
 import "./index.css";
 import App from "./App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ReactLenis
+      root
+      options={{
+        duration: 1.2,
+        smoothWheel: true,
+        wheelMultiplier: 1,
+        touchMultiplier: 2,
+        lerp: 0.1,
+      }}
+    >
+      <App />
+    </ReactLenis>
   </StrictMode>
 );
