d7a7b6b27652fe1fe68bad39bf4f0b0030d76028
Bhagwan Shahay <bhagwanshahay.dev@gmail.com>
2026-08-02 11:43:49 +0530

    Fix Services import

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Hero from "./sections/Hero/Hero";
 import About from "./sections/About/About";
 import FeaturedWork from "./sections/FeaturedWork/FeaturedWork";
-import Services from "./Services/Services";
+import Services from "./sections/Services/Services";
 import Clients from "./sections/Clients/Clients";
 import Contact from "./sections/Contact/Contact";
 
